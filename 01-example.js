cb543fb780f5f1c5acbd65419d03fb661f28e241
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-03-09 11:00:03 +0200

    all

---
+++ 01-example.js
@@ -0,0 +1,111 @@
+// let elemWidth = "50px";
+// const result1 = Number.parseInt(elemWidth);
+// console.log(result1);
+
+// let elemHeight = "200.74px";
+// elemHeight = Number.parseFloat(elemHeight);
+// console.log(elemHeight); //200.74
+
+// let salary = 1200.122457;
+// salary = salary.toFixed(2);
+// salsary = Number(salary);
+// // console.log(salary.toFixed()); //1200.12
+// console.log(salary);
+
+// const base = 2;
+// const power = 5;
+// const result = Math.pow(base, power);
+// console.log(result);
+// console.log(2 ** 3); // 2 в 5-тій степені
+
+// let base = prompt("Write number");
+// base = Number(base);
+
+// let power = prompt("Write degree");
+// power = Number(power);
+
+// const result = base ** power;
+// console.log(result);
+
+// console.log(Math.random() * (max - min) + min);
+
+// -------------------------------------------------------
+
+// const colors = [
+//   "tomato",
+//   "orange",
+//   "black",
+//   "red",
+//   "green",
+//   "yellow",
+//   "blue",
+//   "teal",
+// ];
+
+// const max = colors.length - 1;
+// const min = 0;
+
+// const index = Math.round(Math.random() * (max - min) + min);
+// const color = colors[index];
+// console.log(color);
+
+// document.body.style.backgroundColor = color;
+
+// -------------------------------------------------------
+
+// //конкатинація строк
+// const firstName = "Ali";
+// const lastname = "Liko";
+// const fullName = firstName + " " + lastname;
+// console.log(fullName);
+
+// //шаблонні строки(template strings)
+
+// const greeting = `Welcome ${firstName} ${lastname} to our hotel!`;
+// console.log(greeting);
+
+
+// -------------------------------------------------------
+// //нормалізація з методом toLowerCase()
+// let brand = "Samsung";
+// const normalizedBrand = brand.toLowerCase();
+
+// console.log(normalizedBrand);
+
+// -------------------------------------------------------
+//індексація строки
+
+// let brand = "SaMsUNg";
+// console.log(brand[4]); // даст букву -  u
+// console.log(brand.slice(1).toLowerCase()); // все з 1-ї(по індексу) -та зменшити - amsung
+
+// brand = brand[0] + brand.slice(1).toLowerCase();
+// console.log(brand); // Samsung
+
+
+// -------------------------------------------------------
+//пошук слова в реченні
+
+// const blacklistedWord1 = "спам";
+// const blacklistedWord2 = "розпродаж";
+
+// const string1 = "Привіт, я принц Алі-бі-бі, це не спамб пропоную тобі мільйон";
+// const string2 = " Найбільший РОЗПРОДАЖ цього тижняб не прогав";
+// const string3 = "Рекламна компанія #fatlivesmatter";
+
+// console.log(string1.includes(blacklistedWord1));
+// console.log(string1.toLowerCase().includes(blacklistedWord2));
+
+// -------------------------------------------------------
+
+
+
+
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
+// -------------------------------------------------------
